c7460a6c4f4550e59447aa3ab2196f4bf28574e8
Karam <karam.nassar@hotmail.com>
2025-01-14 19:17:43 +0300

    Fix navbar bg color for mobile

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<ul class="bg-gradient-to-br from-amber-900 via-amber-900 to-amber-900 max-lg:fixed max-lg:top-0 max-lg:left-0 rtl:max-lg:right-0 z-50 max-lg:h-full max-lg:w-1/2 max-lg:overflow-auto lg:bg-none max-lg:p-4 max-lg:shadow-md max-lg:space-y-3 max-lg:min-w-[300px] lg:flex lg:gap-x-8  transition-all duration-500 ease-in-out">
+<ul class="bg-amber-50 max-lg:fixed max-lg:top-0 max-lg:left-0 rtl:max-lg:right-0 z-50 max-lg:h-full max-lg:w-1/4 max-lg:overflow-auto lg:bg-none max-lg:p-4 max-lg:shadow-md max-lg:space-y-3 max-lg:min-w-[300px] lg:flex lg:gap-x-8  transition-all duration-500 ease-in-out">
     <li>
         <a href="{{ route('home') }}" class="py-2 text-sm text-amber-900 relative group">{{ trans('common.home') }}
             <span
